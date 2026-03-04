266e6bfbc294a436ed1e2637f318a521db5e2d20
Kean Allen <keanallenn@gmail.com>
2026-03-04 21:02:31 +0800

    moved db connection to session.py

--- app/core/database.py
+++ app/core/database.py
@@ -1,18 +1,5 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import declarative_base
-from app.core.config import settings
 
-engine = create_async_engine(
-    settings.async_database_url,
-    echo=True,
-    pool_pre_ping=True
-)
-
-AsyncSessionLocal = async_sessionmaker(
-    bind=engine,
-    expire_on_commit=False,
-    class_=AsyncSession
-)
 
 Base = declarative_base()
 
---
+++ app/core/session.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from app.core.config import settings
+
+engine = create_async_engine(
+    settings.async_database_url,
+    echo=True,
+    pool_pre_ping=True
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
