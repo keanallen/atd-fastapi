4fe8feeaa80b03773b644dd3ca1f77851a03f34d
Kean Allen <keanallenn@gmail.com>
2026-03-05 18:33:44 +0800

    Added initial api router prefixes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from app.api.router import api_router
 from app.core.config import settings
 
 app = FastAPI()
+app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/")
 def read_root():
